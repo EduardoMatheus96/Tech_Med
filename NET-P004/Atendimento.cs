7d246584ae5d820d153aff6c2e1322ed038718f7
Welvis <welvisconta@gmail.com>
2023-12-01 20:57:16 -0300

    criação da classe Atendimento, criação dos seus atributos, seters e geters

---
+++ NET-P004/Atendimento.cs
@@ -0,0 +1,68 @@
+public class Atendimento
+{
+
+    public Atendimento()
+    {
+
+    }
+
+    DateTime inicio;
+    DateTime fim;
+    string? suspeitaInicial;
+    // List<Exame> exames = new List<Exame>();
+    float valor;
+    // Medico medicoResponsavel;
+    // Paciente paciente;
+    string? diagnosticoFinal;
+
+    public DateTime Inicio
+    {
+        get { return inicio; }
+        set { inicio = value; }
+    }
+
+    public DateTime Fim
+    {
+        get { return fim; }
+        set { fim = value; }
+    }
+
+    public string SuspeitaInicial
+    {
+        get { return suspeitaInicial!; }
+        set { suspeitaInicial = value; }
+    }
+
+    // public List<Exame> Exames
+    // {
+    //     get { return exames; }
+    //     set { exames = new value; }
+    // }
+
+    public float Valor
+    {
+        get { return valor; }
+        set { valor = value; }
+    }
+
+    // public Medico MedicoResponsavel
+    // {
+    //     get { return medicoResponsavel; }
+    //     set { medicoResponsavel = value; }
+    // }
+
+    // public Paciente Paciente
+    // {
+    //     get { return paciente; }
+    //     set { paciente = value; }
+    // }
+
+    public string DiagnosticoFinal
+    {
+        get { return diagnosticoFinal!; }
+        set { diagnosticoFinal = value; }
+    }
+
+
+
+}
