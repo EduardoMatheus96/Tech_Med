a6a998e7981463e640df4e653005a62eeae4cc24
Eduardo Matheus <eduardomatheusuesc@gmail.com>
2023-12-02 14:39:44 -0300

    Adicionando A Classe Pessoa Feita por Valber na Prova Individual

---
+++ NET-P004/Pessoa.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NET_P004
+{
+    public class Pessoa
+    {
+        private static HashSet<string> cpfsUnicos = new HashSet<string>();
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Cpf { get; set; }
+        public Person(string _name, DateTime _birthDate, string _cpf)
+        {
+            Name = _name;
+            BirthDate = _birthDate;
+
+            if (cpfsUnicos.Add(_cpf))
+            {                       
+                if (ValidateCpf(_cpf))
+                {
+                    Cpf = _cpf;
+                }
+                else
+                {
+                    throw new ArgumentException("O CPF deve conter exatamente 11 dígitos numéricos.");
+
+                }
+            }
+            else
+            {
+                throw new ArgumentException("CPF deve ser único.");
+            }
+        }
+        public int Age => calculateAge(BirthDate);
+
+        public static int calculateAge(DateTime birthDate)
+        {
+            int age = DateTime.Now.Year - birthDate.Year;
+            if (DateTime.Now.DayOfYear < birthDate.DayOfYear)
+                age--;
+            return age;
+        }
+
+        private bool ValidateCpf(string cpf)
+        {
+            if (cpf.Length != 11 || !cpf.All(char.IsDigit))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
