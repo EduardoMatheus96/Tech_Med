4679d5b1e37707ff13d2de60342b702091c0e9af
joaopedropinto <jpcic07@gmail.com>
2023-12-06 10:57:37 -0300

    adicionando construtor

--- NET-P004/CartaoCredito.cs
+++ NET-P004/CartaoCredito.cs
@@ -2,13 +2,19 @@ namespace AvaliacaoGrupo.dotnetP004;
 
 public class CartaoCredito : IPagamento
 {
-  public string Descricao = "Pagamento sendo realizado por Cartão de Credito";
-  public double ValorBruto = 0;
+  public string? Descricao { get; set; }
+  public double ValorBruto { get; set; }
 
-  public double Desconto = 0;
+  public double Desconto { get; set; }
 
-  public DateTime DataHora = DateTime.Now;
+  public DateTime DataHora { get; set; }
 
+   public CartaoCredito(double _ValorBruto){
+            this.Descricao = "Pagamento com cartão de crédito";
+            this.ValorBruto = _ValorBruto;
+            this.DataHora= new DateTime();
+            this.Desconto = 0;
+        }
   public void RealizarPagamento(double valor)
   {
     Console.WriteLine($"Pagamento de {valor} realizado com cartão de crédito");
