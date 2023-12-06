76439beedbcdacc4d316ea94251f8bb83744cf7c
Eduardo Matheus De Menezes Souto <eduardomatheusuesc@gmail.com>
2023-12-06 09:46:32 -0300

    Criando Classse Dinheiro

---
+++ NET-P004/IPagamento.cs
@@ -0,0 +1,14 @@
+namespace AvaliacaoGrupo.dotnetP004
+{
+    public interface IPagamento
+    {
+        public string Descricao { get; set; }
+        public double ValorBruto { get; set; }
+
+        public double Desconto { get; set; }
+
+        public DateTime DataHora { get; set; }
+
+        public void RealizarPagamento(double valor);        
+    }
+}
