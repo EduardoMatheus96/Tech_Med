40888825fd1e9e62d1e3d76a8ad6c07b12c2ed8f
joaopedropinto <jpcic07@gmail.com>
2023-12-06 09:43:17 -0300

    ajustando CartaoCredito

--- NET-P004/CartaoCredito.cs
+++ NET-P004/CartaoCredito.cs
@@ -1,10 +1,17 @@
 namespace AvaliacaoGrupo.dotnetP004;
 
-public class CartaoCredito : IPagamento{
+public class CartaoCredito : IPagamento
+{
   public string Descricao = "Pagamento sendo realizado por Cartão de Credito";
-  public double ValorBruto = 0; 
+  public double ValorBruto = 0;
 
   public double Desconto = 0;
 
-  public DateTime DataHora = DateTime.Now
+  public DateTime DataHora = DateTime.Now;
+
+  public void RealizarPagamento(double valor)
+  {
+    Console.WriteLine($"Pagamento de {valor} realizado com cartão de crédito");
+
+  }
 }
